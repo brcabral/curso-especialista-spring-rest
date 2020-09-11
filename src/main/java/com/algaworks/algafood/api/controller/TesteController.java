6207151e1fee6fbbc838b48717a20b4db2dcf71d
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-09-10 23:31:13 -0300

    Criando consultas com query methods

--- src/main/java/com/algaworks/algafood/api/controller/TesteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -1,16 +1,30 @@
 package com.algaworks.algafood.api.controller;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
 
 @RestController
 @RequestMapping("/teste")
 public class TesteController {
-	// @Autowired
-	// private CozinhaRepository cozinhaRepository;
+	@Autowired
+	private CozinhaRepository cozinhaRepository;
 
-	// @GetMapping("/cozinha/por-nome")
-	// public List<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
-	// return cozinhaRepository.consultarPorNome(nome);
-	// }
+	@GetMapping("/cozinha/por-nome")
+	public List<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome) {
+		return cozinhaRepository.findTodasByNome(nome);
+	}
+
+	@GetMapping("/cozinha/unica-por-nome")
+	public Optional<Cozinha> findByNome(String nome) {
+		return cozinhaRepository.findByNome(nome);
+	}
 }
