9d2faa3d92a45e29187565ed9fecb90ef15370d3
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-09-11 00:04:19 -0300

    Usando as keywords para definir critérios de query methods

--- src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -10,7 +10,7 @@ import com.algaworks.algafood.domain.model.Cozinha;
 
 @Repository
 public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
-	List<Cozinha> findTodasByNome(String nome);
+	List<Cozinha> findTodasByNomeContaining(String nome);
 
 	Optional<Cozinha> findByNome(String nome);
 }
