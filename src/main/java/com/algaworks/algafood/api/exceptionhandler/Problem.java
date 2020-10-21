2e5dc9157ccfc5dc05e31cbe030a5b63cd666bdc
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-10-21 00:14:05 -0300

    Ajustando Exception Handler para adicionar mensagens de validação em nível de classe

--- src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
+++ src/main/java/com/algaworks/algafood/api/exceptionhandler/Problem.java
@@ -20,11 +20,11 @@ public class Problem {
 
 	private LocalDateTime timestamp;
 	private String userMessage;
-	private List<Field> fields;
+	private List<Object> objects;
 
 	@Getter
 	@Builder
-	public static class Field {
+	public static class Object {
 		private String name;
 		private String userMessage;
 	}
