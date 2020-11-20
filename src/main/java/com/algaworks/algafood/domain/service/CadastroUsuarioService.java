e17b6eeae768056f5749c39ecac7de9788385d49
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-11-20 19:49:39 -0300

    Implementando regra de negócio para evitar usuários com e-mails duplicados

--- src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroUsuarioService.java
@@ -1,5 +1,7 @@
 package com.algaworks.algafood.domain.service;
 
+import java.util.Optional;
+
 import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,17 +24,25 @@ public class CadastroUsuarioService {
 
 	@Transactional
 	public Usuario salvar(Usuario usuario) {
+		usuarioRepository.detach(usuario);
+		Optional<Usuario> usuarioExistente = usuarioRepository.findByEmail(usuario.getEmail());
+
+		if (usuarioExistente.isPresent() && !usuarioExistente.get().equals(usuario)) {
+			throw new NegocioException(
+					String.format("Já existe um usuário cadastrado com o e-mail %s", usuario.getEmail()));
+		}
+
 		return usuarioRepository.save(usuario);
 	}
 
 	@Transactional
 	public void alterarSenha(Long usuarioId, String senhaAtual, String novaSenha) {
 		Usuario usuario = buscarOuFalhar(usuarioId);
-		
+
 		if (usuario.senhaNaoCoincideCom(senhaAtual)) {
 			throw new NegocioException("Senha atual informada não coincide com a senha do usuário.");
 		}
-		
+
 		usuario.setSenha(novaSenha);
 	}
 
