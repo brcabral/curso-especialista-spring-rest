6c1031d3ad735e5ec4f97bf6e57a037a175d77d7
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2020-12-30 00:04:52 -0300

    Limitando os campos retornados pela API com Squiggly

--- src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
+++ src/main/java/com/algaworks/algafood/api/model/PedidoResumoModel.java
@@ -3,14 +3,11 @@ package com.algaworks.algafood.api.model;
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
 
-import com.fasterxml.jackson.annotation.JsonFilter;
-
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@JsonFilter("pedidoFilter")
 public class PedidoResumoModel {
 	private String codigo;
 	private BigDecimal subtotal;
