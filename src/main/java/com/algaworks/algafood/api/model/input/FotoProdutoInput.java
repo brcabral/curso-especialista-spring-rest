4c7892815a7739b947a60c314039d43bbfc716da
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-02-11 23:14:20 -0300

    Desafio: Validando o content type do arquivo

--- src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
+++ src/main/java/com/algaworks/algafood/api/model/input/FotoProdutoInput.java
@@ -2,8 +2,10 @@ package com.algaworks.algafood.api.model.input;
 
 import javax.validation.constraints.NotBlank;
 
+import org.springframework.http.MediaType;
 import org.springframework.web.multipart.MultipartFile;
 
+import com.algaworks.algafood.core.validation.FileContentType;
 import com.algaworks.algafood.core.validation.FileSize;
 import com.sun.istack.NotNull;
 
@@ -15,6 +17,7 @@ import lombok.Setter;
 public class FotoProdutoInput {
 	@NotNull
 	@FileSize(max = "500KB")
+	@FileContentType(allowed = { MediaType.IMAGE_JPEG_VALUE, MediaType.IMAGE_PNG_VALUE })
 	private MultipartFile arquivo;
 
 	@NotBlank
