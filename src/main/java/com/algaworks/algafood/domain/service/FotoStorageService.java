f3a99ff9ce4565a97dfc78b58c2fe078948a5aaf
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-02-14 19:40:50 -0300

    Implementando a remoção e substituição de arquivos de fotos no serviço de armazenagem

--- src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FotoStorageService.java
@@ -9,8 +9,18 @@ import lombok.Getter;
 public interface FotoStorageService {
 	void armazenar(NovaFoto novaFoto);
 
+	void remover(String nomeArquivo);
+
 	default String gerarNomeArquivo(String nomeOriginal) {
 		return UUID.randomUUID().toString() + "_" + nomeOriginal;
+	}
+
+	default void substituir(String nomeArquivoAntigo, NovaFoto novaFoto) {
+		this.armazenar(novaFoto);
+
+		if (nomeArquivoAntigo != null) {
+			this.remover(nomeArquivoAntigo);
+		}
 	}
 
 	@Getter
