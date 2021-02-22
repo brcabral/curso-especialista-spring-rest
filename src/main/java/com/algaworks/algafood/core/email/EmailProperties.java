cafdd034b854ca662df8663123a23a2330592f7a
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-02-22 20:03:42 -0300

    Implementando o serviço de infraestrutura de envio de e-mails com Spring

---
+++ src/main/java/com/algaworks/algafood/core/email/EmailProperties.java
@@ -0,0 +1,20 @@
+package com.algaworks.algafood.core.email;
+
+import javax.validation.constraints.NotNull;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Component
+@Validated
+@ConfigurationProperties("algafood.email")
+public class EmailProperties {
+	@NotNull
+	private String remetente;
+}
