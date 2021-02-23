247d3157da24be1ff1ad1c5ab43520faf5c5aade
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-02-23 00:26:09 -0300

    Usando o serviço de envio de e-mails na confirmação de pedidos

--- src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/algaworks/algafood/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -5,14 +5,17 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.stereotype.Service;
 
 import com.algaworks.algafood.core.email.EmailProperties;
 import com.algaworks.algafood.domain.service.EnvioEmailService;
 
+@Service
 public class SmtpEnvioEmailService implements EnvioEmailService {
 	@Autowired
 	private JavaMailSender mailSender;
 
+	@Autowired
 	private EmailProperties emailProperties;
 
 	@Override
