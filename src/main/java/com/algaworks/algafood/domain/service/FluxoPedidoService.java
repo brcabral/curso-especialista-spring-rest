a9e9ce5fba438391e24b7c6afac33cb0663e43c1
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-02-23 22:47:32 -0300

    Processando template do corpo de e-mails com Apache FreeMarker

--- src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/FluxoPedidoService.java
@@ -22,8 +22,8 @@ public class FluxoPedidoService {
 		pedido.confirmar();
 
 		var mensagem = Mensagem.builder().assunto(pedido.getRestaurante().getNome() + " - Pedido confirmado")
-				.corpo("O pedido de código <strong>" + pedido.getCodigo() + "</strong> foi confirmado!")
-				.destinatario(pedido.getCliente().getEmail()).build();
+				.corpo("pedido-confirmado.html").variavel("pedido", pedido).destinatario(pedido.getCliente().getEmail())
+				.build();
 
 		envioEmail.enviar(mensagem);
 	}
