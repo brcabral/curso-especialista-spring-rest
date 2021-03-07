3db629a3b792271cf5c0910568db33eb39278375
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-03-07 16:27:35 -0300

    Descrevendo tags na documentação e associando com controllers

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -9,6 +9,7 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
+import springfox.documentation.service.Tag;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,7 +20,8 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 	@Bean
 	public Docket apiDocket() {
 		return new Docket(DocumentationType.SWAGGER_2).select()
-				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api")).build().apiInfo(apiInfo());
+				.apis(RequestHandlerSelectors.basePackage("com.algaworks.algafood.api")).build().apiInfo(apiInfo())
+				.tags(new Tag("Cidades", "Gerencia as cidades"));
 	}
 
 	public ApiInfo apiInfo() {
