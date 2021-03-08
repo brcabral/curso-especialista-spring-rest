03191b4d9e63b7b201099ccb247ca101e8054df3
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-03-08 00:02:20 -0300

    Desafio: descrevendo documentação de endpoints de cozinhas

--- src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
+++ src/main/java/com/algaworks/algafood/api/model/CozinhaModel.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.model;
 import com.algaworks.algafood.api.model.view.RestauranteView;
 import com.fasterxml.jackson.annotation.JsonView;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -10,8 +11,10 @@ import lombok.Setter;
 @Getter
 public class CozinhaModel {
 	@JsonView(RestauranteView.Resumo.class)
+	@ApiModelProperty(example = "1")
 	private Long id;
 
 	@JsonView(RestauranteView.Resumo.class)
+	@ApiModelProperty(example = "Brasileira")
 	private String nome;
 }
--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -53,8 +53,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
 				.alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Page.class, CozinhaModel.class),
 						CozinhasModelOpenApi.class))
-				.apiInfo(apiInfo())
-				.tags(new Tag("Cidades", "Gerencia as cidades"), new Tag("Grupos", "Gerencia os grupos de usuários"));
+				.apiInfo(apiInfo()).tags(new Tag("Cidades", "Gerencia as cidades"),
+						new Tag("Grupos", "Gerencia os grupos de usuários"),
+						new Tag("Cozinhas", "Gerencia as cozinhas"));
 	}
 
 	private List<ResponseMessage> globalGetResponseMessages() {
