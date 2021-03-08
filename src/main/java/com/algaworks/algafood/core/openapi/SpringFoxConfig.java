1e3c4215fbf5426e1b172e7c596f92b41ca15840
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-03-07 22:59:15 -0300

    Desafio: descrevendo documentação de endpoints de grupos

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -45,7 +45,7 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.useDefaultResponseMessages(false)
 				.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
 				.additionalModels(typeResolver.resolve(Problem.class)).apiInfo(apiInfo())
-				.tags(new Tag("Cidades", "Gerencia as cidades"));
+				.tags(new Tag("Cidades", "Gerencia as cidades"), new Tag("Grupos", "Gerencia os grupos de usuários"));
 	}
 
 	private List<ResponseMessage> globalGetResponseMessages() {
