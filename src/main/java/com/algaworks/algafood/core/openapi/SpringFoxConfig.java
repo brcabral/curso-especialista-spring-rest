151ec40fdc787daae320e31ad87ac5fff6c37fb6
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-03-13 12:08:42 -0300

    Descrevendo parâmetros globais em operações

--- src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
+++ src/main/java/com/algaworks/algafood/core/openapi/SpringFoxConfig.java
@@ -22,6 +22,7 @@ import com.fasterxml.classmate.TypeResolver;
 
 import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
 import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.ParameterBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.builders.ResponseMessageBuilder;
@@ -50,6 +51,9 @@ public class SpringFoxConfig implements WebMvcConfigurer {
 				.globalResponseMessage(RequestMethod.POST, globalPostPutResponseMessages())
 				.globalResponseMessage(RequestMethod.PUT, globalPostPutResponseMessages())
 				.globalResponseMessage(RequestMethod.DELETE, globalDeleteResponseMessages())
+				.globalOperationParameters(Arrays.asList(new ParameterBuilder().name("campos")
+						.description("Nomes das propriedades para filtrar na resposta, separados por vírgula")
+						.parameterType("query").modelRef(new ModelRef("String")).build()))
 				.additionalModels(typeResolver.resolve(Problem.class)).ignoredParameterTypes(ServletWebRequest.class)
 				.directModelSubstitute(Pageable.class, PageableModelOpenApi.class)
 				.alternateTypeRules(AlternateTypeRules.newRule(typeResolver.resolve(Page.class, CozinhaModel.class),
