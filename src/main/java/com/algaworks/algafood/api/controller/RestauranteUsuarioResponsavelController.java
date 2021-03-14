e1e4c915a9d4b1c5feb7a6e8af5f9643bfa94a42
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-03-14 15:05:22 -0300

    Desafio: descrevendo documentação de endpoints de associação de restaurantes com usuários

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,29 +13,33 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.algaworks.algafood.api.assembler.UsuarioModelAssembler;
 import com.algaworks.algafood.api.model.UsuarioModel;
+import com.algaworks.algafood.api.openapi.controller.RestauranteUsuarioResponsavelControllerOpenApi;
 import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 
 @RestController
-@RequestMapping(path = "/restaurantes/{restauranteId}/responsaveis")
-public class RestauranteUsuarioResponsavelController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/responsaveis", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteUsuarioResponsavelController implements RestauranteUsuarioResponsavelControllerOpenApi {
 	@Autowired
 	private CadastroRestauranteService cadastroRestaurante;
 
 	@Autowired
 	private UsuarioModelAssembler usuarioModelAssembler;
 
+	@Override
 	@GetMapping
 	public List<UsuarioModel> listar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
 		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis());
 	}
 
+	@Override
 	@PutMapping("/{usuarioId}")
 	public void associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
 		cadastroRestaurante.associarResponsavel(restauranteId, usuarioId);
 	}
 
+	@Override
 	@DeleteMapping("/{usuarioId}")
 	public void desassociar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
 		cadastroRestaurante.desassociarResponsavel(restauranteId, usuarioId);
