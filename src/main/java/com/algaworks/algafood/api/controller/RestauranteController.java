9f3a6e0ae9ed28110eae6d1d67deaad19a74396a
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-03-13 23:41:31 -0300

    Descrevendo parâmetros de projeções em endpoints de consultas

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteController.java
@@ -21,6 +21,7 @@ import com.algaworks.algafood.api.assembler.RestauranteModelAssembler;
 import com.algaworks.algafood.api.model.RestauranteModel;
 import com.algaworks.algafood.api.model.input.RestauranteInput;
 import com.algaworks.algafood.api.model.view.RestauranteView;
+import com.algaworks.algafood.api.openapi.model.RestauranteBasicoModelOpenApi;
 import com.algaworks.algafood.domain.exception.CidadeNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.CozinhaNaoEncontradaException;
 import com.algaworks.algafood.domain.exception.NegocioException;
@@ -29,6 +30,10 @@ import com.algaworks.algafood.domain.model.Restaurante;
 import com.algaworks.algafood.domain.repository.RestauranteRepository;
 import com.algaworks.algafood.domain.service.CadastroRestauranteService;
 import com.fasterxml.jackson.annotation.JsonView;
+
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping(value = "/restaurantes")
@@ -47,12 +52,17 @@ public class RestauranteController {
 
 	@GetMapping
 	@JsonView(RestauranteView.Resumo.class)
+	@ApiOperation(value = "Lista restaurantes", response = RestauranteBasicoModelOpenApi.class)
+	@ApiImplicitParams({
+			@ApiImplicitParam(value = "Nome da projeção de pedidos", allowableValues = "apenas-nome",
+					name = "projecao", paramType = "query", type = "string") })
 	public List<RestauranteModel> listar() {
 		return restauranteModelAssembler.toCollectionModel(restauranteRepository.findAll());
 	}
 
 	@JsonView(RestauranteView.ApenasNome.class)
 	@GetMapping(params = "projecao=apenas-nome")
+	@ApiOperation(value = "Lista restaurantes", hidden = true)
 	public List<RestauranteModel> listarApenasNomes() {
 		return listar();
 	}
