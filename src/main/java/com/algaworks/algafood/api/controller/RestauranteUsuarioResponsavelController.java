e8adae46d271a73d5e268a299084d9e3187f9b93
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-04-01 22:22:36 -0300

    Desafio: adicionando links de associação de restaurantes com responsáveis

--- src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
+++ src/main/java/com/algaworks/algafood/api/controller/RestauranteUsuarioResponsavelController.java
@@ -3,6 +3,7 @@ package com.algaworks.algafood.api.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.CollectionModel;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -33,19 +34,30 @@ public class RestauranteUsuarioResponsavelController implements RestauranteUsuar
 	@GetMapping
 	public CollectionModel<UsuarioModel> listar(@PathVariable Long restauranteId) {
 		Restaurante restaurante = cadastroRestaurante.buscarOuFalhar(restauranteId);
-		return usuarioModelAssembler.toCollectionModel(restaurante.getResponsaveis()).removeLinks()
-				.add(algaLinks.linkToRestauranteResponsaveis(restauranteId));
+		CollectionModel<UsuarioModel> usuariosModel = usuarioModelAssembler
+				.toCollectionModel(restaurante.getResponsaveis()).removeLinks()
+				.add(algaLinks.linkToRestauranteResponsaveis(restauranteId))
+				.add(algaLinks.linkToRestauranteResponsavelAssociacao(restauranteId, "associar"));
+
+		usuariosModel.getContent().stream().forEach(usuarioModel -> {
+			usuarioModel.add(algaLinks.linkToRestauranteResponsavelDesassociacao(restauranteId, usuarioModel.getId(),
+					"desassociar"));
+		});
+
+		return usuariosModel;
 	}
 
 	@Override
 	@PutMapping("/{usuarioId}")
-	public void associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
+	public ResponseEntity<Void> associar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
 		cadastroRestaurante.associarResponsavel(restauranteId, usuarioId);
+		return ResponseEntity.noContent().build();
 	}
 
 	@Override
 	@DeleteMapping("/{usuarioId}")
-	public void desassociar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
+	public ResponseEntity<Void> desassociar(@PathVariable Long restauranteId, @PathVariable Long usuarioId) {
 		cadastroRestaurante.desassociarResponsavel(restauranteId, usuarioId);
+		return ResponseEntity.noContent().build();
 	}
 }
--- src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
+++ src/main/java/com/algaworks/algafood/api/openapi/controller/RestauranteUsuarioResponsavelControllerOpenApi.java
@@ -1,6 +1,7 @@
 package com.algaworks.algafood.api.openapi.controller;
 
 import org.springframework.hateoas.CollectionModel;
+import org.springframework.http.ResponseEntity;
 
 import com.algaworks.algafood.api.exceptionhandler.Problem;
 import com.algaworks.algafood.api.model.UsuarioModel;
@@ -21,12 +22,12 @@ public interface RestauranteUsuarioResponsavelControllerOpenApi {
 	@ApiOperation("Desassociação de restaurante com usuário responsável")
 	@ApiResponses({ @ApiResponse(code = 204, message = "Desassociação realizada com sucesso"),
 			@ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", response = Problem.class) })
-	void desassociar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
+	ResponseEntity<Void> desassociar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
 			@ApiParam(value = "ID do usuário", example = "1", required = true) Long usuarioId);
 
 	@ApiOperation("Associação de restaurante com usuário responsável")
 	@ApiResponses({ @ApiResponse(code = 204, message = "Associação realizada com sucesso"),
 			@ApiResponse(code = 404, message = "Restaurante ou usuário não encontrado", response = Problem.class) })
-	void associar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
+	ResponseEntity<Void> associar(@ApiParam(value = "ID do restaurante", example = "1", required = true) Long restauranteId,
 			@ApiParam(value = "ID do usuário", example = "1", required = true) Long usuarioId);
 }
