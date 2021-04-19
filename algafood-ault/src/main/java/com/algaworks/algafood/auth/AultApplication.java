af14916803e4f069a7ae2a276845277222bb57c7
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-04-18 23:09:33 -0300

    Criando o projeto do Authorization Server com Spring Security OAuth2

---
+++ algafood-ault/src/main/java/com/algaworks/algafood/auth/AultApplication.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AultApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AultApplication.class, args);
+	}
+
+}
