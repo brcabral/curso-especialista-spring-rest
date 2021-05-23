92a4c1f96b50627205fc13e299d2cefbd4d0d3d5
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-05-22 23:34:46 -0300

    Carregando as permissões concedidas na autenticação

--- algafood-ault/src/main/java/com/algaworks/algafood/auth/domain/Grupo.java
+++ algafood-ault/src/main/java/com/algaworks/algafood/auth/domain/Grupo.java
@@ -1,4 +1,4 @@
-package com.algaworks.algafood.domain.model;
+package com.algaworks.algafood.auth.domain;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -30,12 +30,4 @@ public class Grupo {
 	@ManyToMany
 	@JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"), inverseJoinColumns = @JoinColumn(name = "permissao_id"))
 	private Set<Permissao> permissoes = new HashSet<>();
-
-	public boolean adicionarPermissao(Permissao permissao) {
-		return getPermissoes().add(permissao);
-	}
-
-	public boolean removerPermissao(Permissao permissao) {
-		return getPermissoes().remove(permissao);
-	}
 }
--- algafood-ault/src/main/java/com/algaworks/algafood/auth/domain/Permissao.java
+++ algafood-ault/src/main/java/com/algaworks/algafood/auth/domain/Permissao.java
@@ -1,16 +1,10 @@
 package com.algaworks.algafood.auth.domain;
 
-import java.util.HashSet;
-import java.util.Set;
-
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
 
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -18,7 +12,7 @@ import lombok.EqualsAndHashCode;
 @Data
 @EqualsAndHashCode(onlyExplicitlyIncluded = true)
 @Entity
-public class Usuario {
+public class Permissao {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@EqualsAndHashCode.Include
@@ -26,14 +20,7 @@ public class Usuario {
 
 	@Column(nullable = false)
 	private String nome;
-
+	
 	@Column(nullable = false)
-	private String email;
-
-	@Column(nullable = false)
-	private String senha;
-
-	@ManyToMany
-	@JoinTable(name = "usuario_grupo", joinColumns = @JoinColumn(name = "usuario_id"), inverseJoinColumns = @JoinColumn(name = "grupo_id"))
-	private Set<Grupo> grupos = new HashSet<>();
+	private String descricao;
 }
