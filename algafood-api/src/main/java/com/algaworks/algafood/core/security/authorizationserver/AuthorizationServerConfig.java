1c1ed4583de3cdd717f5de102765e220fe3ee720
Breno Rodrigo Cabral de Oliveira <brcabral@gmail.com>
2021-06-08 23:06:00 -0300

    Resolvendo problemas com storage de Authorization Codes

--- algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
+++ algafood-api/src/main/java/com/algaworks/algafood/core/security/authorizationserver/AuthorizationServerConfig.java
@@ -20,6 +20,7 @@ import org.springframework.security.oauth2.provider.CompositeTokenGranter;
 import org.springframework.security.oauth2.provider.TokenGranter;
 import org.springframework.security.oauth2.provider.approval.ApprovalStore;
 import org.springframework.security.oauth2.provider.approval.TokenApprovalStore;
+import org.springframework.security.oauth2.provider.code.JdbcAuthorizationCodeServices;
 import org.springframework.security.oauth2.provider.token.TokenEnhancerChain;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JwtAccessTokenConverter;
@@ -56,7 +57,8 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
 		enhancerChain.setTokenEnhancers(Arrays.asList(new JwtCustomClaimsTokenEnhancer(), jwtAccessTokenConverter()));
 
 		endpoints.authenticationManager(authenticationManager).userDetailsService(userDetailsService)
-				.reuseRefreshTokens(false).accessTokenConverter(jwtAccessTokenConverter()).tokenEnhancer(enhancerChain)
+				.authorizationCodeServices(new JdbcAuthorizationCodeServices(this.dataSource)).reuseRefreshTokens(false)
+				.accessTokenConverter(jwtAccessTokenConverter()).tokenEnhancer(enhancerChain)
 				.approvalStore(approvalStore(endpoints.getTokenStore())).tokenGranter(tokenGranter(endpoints));
 	}
 
